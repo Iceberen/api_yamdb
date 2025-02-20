d7b488e775197f03d2a7994192560e950682d3b7
Konstantin Ananev <russrage@yandex.ru>
2025-02-20 15:35:42 +0300

    add register_user

--- api_yamdb/api/v1/auth/serializers.py
+++ api_yamdb/api/v1/auth/serializers.py
@@ -1,19 +1,44 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
+
+
 User = get_user_model()
 
-class SignupSerializer(serializers.ModelSerializer):
+class SignupSerializer(serializers.Serializer):
 
-    class Meta:
-        model = User
-        fields = ('username', 'email')
+    email = serializers.EmailField(max_length=254, required=True)
+    username = serializers.RegexField(r'^[\w.@+-]+$', max_length=150, required=True)
 
+    def validate_username(self, value):
+        """Запрещаем использовать 'me' в качестве имени пользователя."""
+        if value.lower() == 'me':
+            raise serializers.ValidationError('Нельзя использовать "me" в качестве username.')
+        return value
 
     def validate(self, attrs):
-        instance = User(**attrs)
-        try:
-            instance.clean()
-        except ValidationError as e:
-            raise serializers.ValidationError(e.args[0])
+        """Глобальная валидация: запрещаем использование email и username, если они принадлежат разным пользователям."""
+        email = attrs["email"]
+        username = attrs["username"]
+
+        user_with_email = User.objects.filter(email=email).first()
+        user_with_username = User.objects.filter(username=username).first()
+
+        if user_with_email and user_with_email.username != username:
+            raise serializers.ValidationError({"email": "Этот email уже используется другим пользователем."})
+
+        if user_with_username and user_with_username.email != email:
+            raise serializers.ValidationError({"username": "Этот username уже используется другим пользователем."})
+
         return attrs
+
+    def create(self, validated_data):
+        """Создаём пользователя, если его нет, или возвращаем существующего."""
+        user, created = User.objects.get_or_create(
+            email=validated_data['email'],
+            defaults={"username": validated_data['username']}
+        )
+
+        if not created and user.username != validated_data['username']:
+            raise serializers.ValidationError({"email": "Этот email уже используется другим пользователем."})
+
+        return user
--- api_yamdb/api/v1/auth/views.py
+++ api_yamdb/api/v1/auth/views.py
@@ -1,25 +1,30 @@
 from http import HTTPStatus
-
+from django.db.utils import IntegrityError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.v1.auth.serializers import SignupSerializer
-from api.v1.auth.utils import generate_confirmation_code, send_confirmation_email
+from api.v1.auth.utils import send_confirmation_email
 
 
 class SignupView(APIView):
     def post(self, request):
         serializer = SignupSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
+            try:
+                user = serializer.save()
+                send_confirmation_email(username=user.username, email=user.email)
 
-            send_confirmation_email(username=user.username, email=user.email)
+                context = {
+                    'email': user.email,
+                    'username': user.username,
+                }
 
-            context = {
-                'email': user.email,
-                'username': user.username,
-            }
+                return Response(context, status=HTTPStatus.OK)
+            except IntegrityError:
+                return Response(
+                    {"error": "Ошибка базы данных. Попробуйте снова."},
+                    status=HTTPStatus.INTERNAL_SERVER_ERROR
+                )
 
-
-            return Response(context, status=HTTPStatus.OK)
         return Response(serializer.errors, status=HTTPStatus.BAD_REQUEST)
